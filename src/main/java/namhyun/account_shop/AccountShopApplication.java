fde35f41f89cd582e5bc7efbbcd28950f3623257
drssee <sarmos88@naver.com>
2024-06-21 09:47:14 +0900

    프로젝트 세팅

---
+++ src/main/java/namhyun/account_shop/AccountShopApplication.java
@@ -0,0 +1,13 @@
+package namhyun.account_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/namhyun/account_shop/AccountShopApplicationTests.java
@@ -0,0 +1,13 @@
+package namhyun.account_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
